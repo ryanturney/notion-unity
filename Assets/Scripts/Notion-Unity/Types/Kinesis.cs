e6f8edacdea16601e1350cbf447860166501caee
Dilmer Valecillos <dilmervst@gmail.com>
2022-01-22 12:56:51 -0700

    feature(kinesis): added kinesis handler, kinesis type, and kinesis subscription button and text to sample scene

---
+++ Assets/Scripts/Notion-Unity/Types/Kinesis.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Notion.Unity
+{
+    public class Kinesis
+    {
+        public float Confidence { get; set; }
+
+        public string Label { get; set; }
+
+        public string Metric { get; set; }
+
+        public IEnumerable<BaseMetric> Predictions { get; set; }
+
+        public int Streak { get; set; }
+
+        public long Timestamp { get; set; }
+    }
+}
